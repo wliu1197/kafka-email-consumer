62bb71cece02d64bcba0fce50bb5ad2c9bc92585
wenhaoliu <wliu1197@uni.sydney.edu.au>
2025-02-05 13:35:44 +1100

    ErrorHandlingDeserializer If there's deserialization error happens in consumer side consumer will throw exception without keep retrying and allows consumer to continue handle next messages from broker topic

--- src/main/java/com/kafka/ms/email/config/KafkaConsumerConfiguration.java
+++ src/main/java/com/kafka/ms/email/config/KafkaConsumerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -29,9 +30,13 @@ public class KafkaConsumerConfiguration {
         Map<String,Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,keyDeserializer);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,valueDeserializer);
+       // config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,valueDeserializer);
         config.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         config.put(JsonDeserializer.TRUSTED_PACKAGES,trustedPackage);
+        // to Handler Deserializer error using bean config
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
+
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
